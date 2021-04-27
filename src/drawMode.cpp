f832ee0c2d2a8548ce006af77a03e0ee77771f2d
AngelGonzs <angel.gonzalez49@upr.edu>
2021-04-26 17:23:16 -0700

    mode1 fixed yea yea it fits standards now yea yea

--- src/drawMode.cpp
+++ src/drawMode.cpp
@@ -6,12 +6,15 @@ void DM1::draw(int x,int y, int n, int radius){
     if(n!=0){
         ofSetColor(x/4, y/4, n*64 );
 
-        int rD = radius/n; //this alters the position of x/y and decreases the radius
-        //to an extent, it serves as a complementary value to radius
         ofDrawCircle(x, y, radius);
-        draw(x+radius - rD , y, n-1, radius - rD );
-        draw(x-radius + rD , y, n-1, radius - rD );
-        draw(x, y+radius - rD , n-1, radius - rD );
-        draw(x, y-radius + rD , n-1, radius - rD );
+        draw(x+radius , y, n-1, radius/2 );
+        draw(x-radius , y, n-1, radius/2 );
+        draw(x, y+radius , n-1, radius/2 );
+        draw(x, y-radius , n-1, radius/2);
     }
 }
+
+        // drawMode1(x+100, y, n-1);
+        // drawMode1(x-100, y, n-1);
+        // drawMode1(x, y+100, n-1);
+        // drawMode1(x, y-100, n-1);
