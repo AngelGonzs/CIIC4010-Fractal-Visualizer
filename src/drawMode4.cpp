ff44d294d28b621e9babb523c4f6165c9f6a8493
AngelGonzs <angel.gonzalez49@upr.edu>
2021-04-28 12:15:48 -0700

    added color to DM4

--- src/drawMode4.cpp
+++ src/drawMode4.cpp
@@ -4,7 +4,13 @@ DM4::DM4(bool activate): Fractal(activate){}
 
 void DM4::draw(float x, float y, float size, int n){
     if(n!=0){
-        // ofSetColor(x+(n+5)*10,y+(n+5)*10,size*5);
+        int color = x+y;
+
+        if(color%5==0) ofSetColor(255,0,0); 
+        else if (color%3==0) ofSetColor(255, 255, 0);
+        else if (color%2==0) ofSetColor(0,0,255);
+        else ofSetColor(0,255,0);
+        //setting colors of course yea yea
         ofDrawRectangle(x,y,size,size);
         for(int i=-1;i<2;i++){
             for(int j=-1;j<2;j++){
